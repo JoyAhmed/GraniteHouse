5592b5e37d30483f5b64af659bd94b6f27fd399b
JoyAhmed <joy_ahmed_007@yahoo.com>
2019-03-10 07:47:46 +0600

    Initial commit

---
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.ProductTypes.ToList());
+        }
+
+        //GET Create Action Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Create action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productTypes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
+        }
+    }
+}
